06ded815e766f97100b2a2f480ca93d4dda21f15
Jason Yu <jasonyu@csus.edu>
2022-11-27 22:21:14 -0800

    Fancify Text

---
+++ FancifyMyText.js
@@ -0,0 +1,29 @@
+function enlargeText(){
+    var txt = document.getElementById("textInput");
+    txt.style.fontSize = "24pt";
+}
+
+function normalText(){
+    var txt = document.getElementById("textInput");
+    txt.style.fontSize = "10pt";
+}
+
+function fancify(){
+    var txt = document.getElementById("textInput");
+    var fancy = document.getElementById("fancy").checked;
+    if(fancy){
+        txt.style.fontWeight = "bold";
+        txt.style.color = "blue";
+        txt.style.textDecoration = "underline";
+    }else{
+        txt.style.fontWeight = "normal";
+        txt.style.color = "black";
+        txt.style.textDecoration = "none";
+    }
+}
+
+function moo(){
+    var txt = document.getElementById("textInput");
+    var split = txt.ariaValueMax.split(".");
+    txt.value = split.join("-Moo.");
+}

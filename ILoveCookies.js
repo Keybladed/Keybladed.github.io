fd2c3c597cced649fbe9b9a783fcb44b45ad5bc6
Jason Yu <jasonyu@Jasons-MacBook-Pro-2.local>
2022-12-05 19:17:35 -0800

    fixed issues with Assignment 9 and added styles to it

--- ILoveCookies.js
+++ ILoveCookies.js
@@ -9,28 +9,39 @@
         savedIP();
     }
 
-    var count = 1;
     function clear() {
         localStorage.clear();
         localStorage.count = 0;
         savedIP();
     }
-    
-    function savedIP(){
-        let ip = document.getElementById("ip");
-        let str = "";
-
-        if(!localStorage.count){
-            localStorage.count = 1;
-        }else{
-            localStorage.count++;
-        }
-
-        localStorage.setItem(localStorage.count, ip.textContent);
-
-        for(let i = 1; i < localStorage.count; i++){
-            str += localStorage.getItem(i) + "<br>";
-        }
-        document.getElementById("saved").innerHTML = str;
-    }
 })();
+
+const api_url = "https://api.ipify.org";
+
+function savedIP(){
+    let ip = document.getElementById("ip");
+    let str = "";
+    
+    if(!localStorage.count){
+        localStorage.count = 1;
+    }else{
+        localStorage.count++;
+    }
+
+    let temp = getapi(api_url);
+
+    for(let i = 1; i < localStorage.count; i++){
+        str += localStorage.getItem(i) + "<br>";
+    }
+    document.getElementById("saved").innerHTML = str;
+}
+
+async function getapi(url) {
+    // Storing response
+    const response = await fetch(url);
+    // Storing data in form of JSON
+    var data = await response.text();
+    console.log(data);
+    localStorage.setItem(localStorage.count, data);
+    return data;
+}
